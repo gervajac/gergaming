d65c63cc54f7d1840853ee3e8cbab9f88a79a8b4
gervajac <gerva.jacob.tropini@gmail.com>
2023-04-11 19:38:32 -0300

    fixfilter

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -11,28 +11,26 @@ import desc from "../../assets/sortd.svg";
 export interface SideBarProps {}
 
 const SideBar: React.FC<SideBarProps> = () => {
-  const icon1 = document.getElementById("icon1");
-  const menu1 = document.getElementById("menu1");
 
-  // const showMenu1 = (flag) => {
-  //   if (flag) {
-  //     icon1.classList.toggle("rotate-180");
-  //     menu1.classList.toggle("hidden");
-  //   }
-  // };
 
-  let Main = document.getElementById("Main");
-  let open = document.getElementById("open");
-  let close = document.getElementById("close");
+let icon1 = document.getElementById("icon1");
+let menu1 = document.getElementById("menu1");
+const showMenu1 = (flag) => {
+  if (flag) {
+    icon1.classList.toggle("rotate-180");
+    menu1.classList.toggle("hidden");
+  }
+};
 
-  // const showNav = (flag) => {
-  //   if (flag) {
-  //     Main.classList.toggle("-translate-x-full");
-  //     Main.classList.toggle("translate-x-0");
-  //     open.classList.toggle("hidden");
-  //     close.classList.toggle("hidden");
-  //   }
-  // };
+let icon2 = document.getElementById("icon2");
+let menu2 = document.getElementById("menu2");
+
+const showMenu2 = (flag) => {
+  if (flag) {
+    icon2.classList.toggle("rotate-180");
+    menu2.classList.toggle("hidden");
+  }
+};
 
   return (
     <>
@@ -86,7 +84,7 @@ const SideBar: React.FC<SideBarProps> = () => {
         </div>
         <div className="flex flex-col justify-start items-center   px-6 border-b border-gray-600 w-full  ">
           <button
-            // onClick={() => showMenu1(false)}
+            onClick={() => showMenu1(true)}
             className="focus:outline-none focus:text-indigo-400 text-left  text-white flex justify-between items-center w-full py-5 space-x-14  "
           >
             <p className="text-sm leading-5  uppercase">Categorias</p>
@@ -110,7 +108,7 @@ const SideBar: React.FC<SideBarProps> = () => {
           </button>
           <div
             id="menu1"
-            className="flex justify-start  flex-col w-full md:w-auto items-start pb-1 "
+            className="justify-start hidden  flex-col w-full md:w-auto items-start pb-1 "
           >
             <button className="flex justify-start items-center space-x-6 hover:text-white focus:bg-gray-700 focus:text-white hover:bg-gray-700 text-gray-400 rounded px-3 py-2  w-full md:w-52">
               <img src={mother}></img>
@@ -140,12 +138,12 @@ const SideBar: React.FC<SideBarProps> = () => {
         </div>
         <div className="flex flex-col justify-start items-center   px-6 border-b border-gray-600 w-full  ">
           <button
-            // onClick={() => showMenu1(false)}
+            onClick={() => showMenu2(true)}
             className="focus:outline-none focus:text-indigo-400 text-left  text-white flex justify-between items-center w-full py-5 space-x-14  "
           >
             <p className="text-sm leading-5  uppercase">Precio</p>
             <svg
-              id="icon1"
+              id="icon2"
               className="transform"
               width="24"
               height="24"
@@ -163,8 +161,8 @@ const SideBar: React.FC<SideBarProps> = () => {
             </svg>
           </button>
           <div
-            id="menu1"
-            className="flex justify-start  flex-col w-full md:w-auto items-start pb-1 "
+            id="menu2"
+            className="hidden justify-start  flex-col w-full md:w-auto items-start pb-1 "
           >
             <button className="flex justify-start items-center space-x-6 hover:text-white focus:bg-gray-700 focus:text-white hover:bg-gray-700 text-gray-400 rounded px-3 py-2  w-full md:w-52">
               <img src={desc}></img>
@@ -177,8 +175,12 @@ const SideBar: React.FC<SideBarProps> = () => {
           </div>
         </div>
       </div>
+      
     </>
+    
   );
 };
+
+
 
 export default SideBar;
